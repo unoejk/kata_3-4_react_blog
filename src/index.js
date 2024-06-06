b91b90488c761454f0b19589cf6e29b8a2847501
Leonid Burtsev <unoejk@gmail.com>
2024-06-07 00:04:35 +0300

    tempCheck stileWeWillWorkingWithTishIThink

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+// react
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// store
 import {Provider} from 'react-redux'
 import store from './store/store'
+// router
 import {BrowserRouter as Router} from 'react-router-dom'
-import './styles/reset.css'
-import './styles/default.scss'
+// styles
+import './styles/resetDownloaded.css'
+import './styles/resetMinePlus.scss'
+import './styles/quickClasses.scss'
+import './styles/antdFix.scss'
+// components
 import App from './components/App/App'
 
+// go-go
 const root=ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <Provider store={store}>
